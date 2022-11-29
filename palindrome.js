a3bbdfbafaf33cee51f097ef4f5e7371b88b8e10
Yogeshsorde <dineshsorde1997@gmail.com>
2022-11-29 22:40:02 +0530

    check the palindrome & prime question

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+let str="naman"
+
+let bag=""
+
+for(let i<str.length-1;i>0;i--){
+
+    bag=bag+str[j]
+
+}
+
+if(bag==str){
+
+console.log("yes")
+
+}
+
+else{
+
+console.log("no")
+
+
+
---
+++ prime.js
@@ -0,0 +1,23 @@
+let num=13
+
+
+let count=0
+
+for(let i=0;i<=num;i++){
+
+  if(num%i!==0){
+
+count+
+
+}
+
+if(count=1){
+
+console.log("true")
+
+
+}
+
+else{
+
+console.log("false")
